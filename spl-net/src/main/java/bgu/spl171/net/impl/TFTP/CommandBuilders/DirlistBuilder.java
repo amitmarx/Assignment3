9230163c203c22cd47735ac8165ae7baebf78fe8
Amit Marx <amitm@wix.com>
2017-01-17 18:43:59 +0200

    fix structure

--- spl-net/src/main/java/bgu/spl171/net/impl/TFTP/CommandBuilders/DirlistBuilder.java
+++
@@ -1,5 +0,0 @@
-package bgu.spl171.net.impl.TFTP.CommandBuilders;
-
-public class DirlistBuilder {
-
-}
--- spl-net/src/main/java/bgu/spl171/net/impl/TFTP/Commands/Responses/Response.java
+++
@@ -1,6 +0,0 @@
-package bgu.spl171.net.impl.TFTP.Commands.Responses;
-
-import bgu.spl171.net.impl.TFTP.Commands.Command;
-
-public interface Response extends Command {
-}
